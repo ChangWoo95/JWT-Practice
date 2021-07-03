28708199abe897c07ee17534d8e719100b50d0a8
DESKTOP-8H087C4\jung <qaqa313a@gmail.com>
2021-07-03 15:48:10 +0900

    Feat: Security config setting(init)

---
+++ jwt-practice/src/main/java/me/changwoo/practice/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package me.changwoo.practice.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정하겠다
+                .antMatchers("/api/hello").permitAll() // 괄호 안에 들어간 요청에 대해서는 인증없이 접근을 허용해주겠다
+                .anyRequest().authenticated(); // 나머지에 대해서는 모두 인증이 필요하다!
+    }
+}
